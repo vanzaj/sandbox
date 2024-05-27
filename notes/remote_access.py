9428596add69a13e3305b0ceb9806da70fc6714b
Ivan <vanzaj@gmail.com>
2024-05-27 15:23:17 +0800

    github api

--- notes/remote_access.py
+++ notes/remote_access.py
@@ -1,5 +1,19 @@
-import os
+import base64
 from dotenv import load_dotenv
+from github import Github
+import os
+
 load_dotenv()
-token = os.getenv("SANDBOX_TOKEN")
-print(token)
+access_token = os.getenv("SANDBOX_TOKEN")
+g = Github(access_token)
+
+repo = g.get_repo("vanzaj/sandbox")
+
+contents = repo.get_contents("")
+while contents:
+    file = contents.pop(0)
+    if file.type == "dir":
+        contents.extend(repo.get_contents(file.path))
+    else:
+        print(file)
+
