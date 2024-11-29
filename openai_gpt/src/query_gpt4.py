2bdb8e4a1a6df8a7604c4ade9186f2f870851915
Ivan <vanzaj@gmail.com>
2024-11-30 01:03:17 +0900

    cleanup

--- openai_gpt/src/query_gpt4.py
+++ openai_gpt/src/query_gpt4.py
@@ -2,32 +2,38 @@ from api import client
 
 model = "chatgpt-4o-latest"
 
-messages = [
-    {
-    "role": "system",
-    "content": "you are an expert XP developer"
-    },
-    {"role": "user",
-     "content": "Describe common code smells using Python code as examples"}
-]
 
-response = client.chat.completions.create(model=model, messages=messages)
+def single_query():
+    messages = [
+        {"role": "system", "content": "you are an expert XP developer"},
+        {
+            "role": "user",
+            "content": "Describe common code smells using Python code as examples",
+        },
+    ]
 
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(model=model, messages=messages)
 
-print("\n=== Generate N responses ===\n")
+    print(response.choices[0].message.content)
 
-messages = [
-    {
-    "role": "system",
-    "content": "you are an expert XP developer"
-    },
-    {"role": "user",
-     "content": "Describe the meaning of Collective Code Ownership"}
-]
 
-response = client.chat.completions.create(model=model, messages=messages, n=2, stop=["\n"])
-for choice in response.choices:
-    print(f"Choice:{choice.index}")
-    print(choice.message.content)
-    print()
+def multiple_query():
+    messages = [
+        {"role": "system", "content": "you are an expert XP developer"},
+        {
+            "role": "user",
+            "content": "Describe the meaning of Collective Code Ownership",
+        },
+    ]
+
+    response = client.chat.completions.create(
+        model=model, messages=messages, n=2, stop=["\n"]
+    )
+    for choice in response.choices:
+        print(f"Choice:{choice.index}")
+        print(choice.message.content)
+        print()
+
+
+if __name__ == "__main__":
+    multiple_query()
