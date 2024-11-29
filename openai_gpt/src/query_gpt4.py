a37930f0cc57f1084c8f847f695afa44c37e0746
Ivan <vanzaj@gmail.com>
2024-11-29 14:03:39 +0900

    query gpt4

--- openai_gpt/src/query_gpt4.py
+++ openai_gpt/src/query_gpt4.py
@@ -14,3 +14,20 @@ messages = [
 response = client.chat.completions.create(model=model, messages=messages)
 
 print(response.choices[0].message.content)
+
+print("\n=== Generate N responses ===\n")
+
+messages = [
+    {
+    "role": "system",
+    "content": "you are an expert XP developer"
+    },
+    {"role": "user",
+     "content": "Describe the meaning of Collective Code Ownership"}
+]
+
+response = client.chat.completions.create(model=model, messages=messages, n=2, stop=["\n"])
+for choice in response.choices:
+    print(f"Choice:{choice.index}")
+    print(choice.message.content)
+    print()
